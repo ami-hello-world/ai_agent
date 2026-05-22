17dcae74d8207270c1f7dc61ab0aa647a690377c
ami_hello_world <touroku0408@gmail.com>
2026-05-22 16:35:15 +0900

    FIX

--- main.py
+++ main.py
@@ -1,84 +1,36 @@
 import asyncio
-from DB import commit_db,create_table
-from vector import create_index, change_vector, vector_search
-from voice import text_to_voice
-from config import OPENAI_API_KEY, DB_PATH
-import openai
-import sqlite3
 import json
 import os
-
-create_table()
-
-def get_related_texts(ids):
-    conn = sqlite3.connect(DB_PATH)
-    cursor = conn.cursor()
-    results = []
-    for id_ in ids:
-        cursor.execute("SELECT dialogue FROM chatlog WHERE id = ?", (id_,))
-        row = cursor.fetchone()
-        if row:
-            dialogue = json.loads(row[0])  # これはリスト形式
-            if isinstance(dialogue, list):
-                results.extend(dialogue)  # リストを展開して追加
-            else:
-                results.append(dialogue)  # 念のため辞書も対応
-    conn.close()
-    return results
-
-def generate_response(related_texts, user_input):
-    openai.api_key = OPENAI_API_KEY
-
-    messages = [{"role": "system", "content": "あなたは親切で賢いアシスタントです。曖昧な質問に関しては意図をさぐるように返してください。"}]
-
-    for text in related_texts:
-        if isinstance(text, dict) and "role" in text and "content" in text:
-            messages.append(text)
-        else:
-            print("無効なメッセージ形式:", text)
-
-    messages.append({"role": "user", "content": user_input})
-
-    response = openai.ChatCompletion.create(
-        model="gpt-4",
-        messages=messages
-    )
-
-    return response["choices"][0]["message"]["content"]
+from openai import AsyncOpenAI
+from mcp.client.stdio import stdio_client, StdioServerParameters
+from mcp.client.session import ClientSession
+from langchain_openai import ChatOpenAI
+from langchain_core.tools import Tool
 
 
-async def main():
-    
-    index = create_index()  # FAISSインデックス生成（起動時1回）
 
-    while True:
-        user_input = input(">>> あなた：")
+async def main( ):
+    server_parameters = StdioServerParameters(command="python",args=["server.py"])
+    async with stdio_client(server_parameters) as (read, write):
+        async with ClientSession(read, write) as session:
+            await session.initialize()
+            mcp_tools_response = await session.list_tools()
+            langchain_tool = []
+            for i in mcp_tool_response.tool:
+                tool = Tool(
+                    name = i.name,
+                    description = i.description,
+                    func = lambda **kwargs: asyncio.run(session.call_tool(i.name, arguments=kwargs))
+                )
+                langchain_tool.append(tool)
 
-        if user_input.lower() in ["exit", "bye"]:
-            print("アシスタント：またお話しましょう！")
-            await text_to_voice("終了")
-            break           
+            llm = ChatOpenAI(model="gpt-4o", api_key="YOUR_KEY")
+            llm_with_tools = llm.bind_tools(langchain_tool)
+            while True:
+                text = input("your prompt here")
+                response = await llm_with_tools.invoke(text)
+                print(response.tool_calls)
+                print(response.content)
 
-        if os.path.exists('Rlog.json') and os.stat('Rlog.json').st_size > 0:
-            with open('Mistory.json', 'r', encoding='utf-8') as f:
-                dict_json = json.load(f)
-                messages = user_input
-                messages.extend(dict_json) 
-
-
-        vector = change_vector(user_input)
-        ids = vector_search(user_input, index, k=10)
-        print(ids)
-        related_texts = get_related_texts(ids)
-        print(related_texts)
-        response = generate_response(related_texts, user_input)
-
-        print("アシスタント：", response)
-        await text_to_voice(response)
-        dialogue = [{"role": "user", "content": user_input}, {"role": "assistant", "content": response}]
-        dialogue_json = json.dumps(dialogue, ensure_ascii=False)
-        commit_db(vector.tolist(), dialogue_json)
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
+if __name__ =="__main__":
+     asyncio.run(main())
